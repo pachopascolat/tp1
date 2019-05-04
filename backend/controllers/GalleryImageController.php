c1a0d338ad99c9fa7f21f305a4b20f08aabef080
pachopascolat <patriciopascolat@gmail.com>
2019-05-04 20:28:40 -0300

    intentando que funcione la conversion a pdf

--- backend/controllers/GalleryImageController.php
+++ backend/controllers/GalleryImageController.php
@@ -93,7 +93,7 @@ class GalleryImageController extends Controller {
         }
 
         $options = [
-            'binary' => 
+//            'binary' => 
             'page-size' => 'A4',
 //            'header-html' => $this->renderPartial('_pdfHeader'),
 //            'footer-html' => $this->renderPartial('_pdfFooter'),
