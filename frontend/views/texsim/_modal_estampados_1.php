56bf2a5c05bde1ab8b1bed34fbfac9979fda3390
pachopascolat <patriciopascolat@gmail.com>
2019-07-31 19:33:44 -0300

    probando visualizacion 2

--- frontend/views/texsim/_modal_estampados_1.php
+++ frontend/views/texsim/_modal_estampados_1.php
@@ -1,8 +1,9 @@
-<!--modal de cada liso-->
-<?php if ($model->liso != null && count($model->liso->disenios[0]->getBehavior('galleryBehavior')->getImages()) > 0): ?>
-    <?php foreach ($model->liso->disenios[0]->getBehavior('galleryBehavior')->getImages() as $lisos) : ?>
+<?php
+foreach ($model->getSliders() as $key => $galeria):
+    foreach ($galeria as $index => $dis):
+        ?>
 
-        <div id="exampleModal-<?= $lisos->id ?>" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade quickview">
+        <div id="exampleModal-<?= $dis->id ?>" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade quickview">
             <div role="document" class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <button type="button" data-dismiss="modal" aria-label="Close" class="close modal-close">
@@ -10,36 +11,31 @@
                         <use xlink:href="#close-1"> </use>
                         </svg>
                     </button>
-                    <div class="modal-body">
+                    <div class="modal-body"> 
+                        
                         <?php
-                        $img = \common\models\GalleryImage::findOne($lisos->id);
-                        if ($img->agotado):
-                            ?>
-                            <div class="ribbon ribbon-primary">
-                                Agotado
-                            </div>
-
-                            <?php
-                        elseif ($img->oferta):
-                            ?>
-                            <div class="ribbon ribbon-primary">
-                                Oferta
-                            </div>
-
-                        <?php endif;
+                        $img = $dis;
                         ?>
+                        <div class="ribbon ribbon-agotado <?=!$img->agotado? "d-none":""?>">
+                            Agotado
+                        </div>
+                        <div class="ribbon ribbon-oferta <?=!$img->oferta? "d-none":""?>">
+                            Oferta
+                        </div>
 
                         <div class="row">
                             <div class="col-lg-6">
-                                <div id="" data-slider-id="1" class="owl-carousel owl-theme owl-dots-modern detail-full owl">
-                                    <div data-code="<?= $lisos->id ?>" id="modal-img<?= $lisos->id ?>" style="background: center center  url('<?= $lisos->getUrl('original') ?>') no-repeat; background-size: cover;" class="detail-full-item-modal"></div>
+                                <div id="modelos-slider<?= $dis->id ?>" data-slider-id="<?= $dis->id ?>" class="owl-carousel owl-theme owl-dots-modern detail-full owl">
+                                    <div data-agotado="<?= $img->agotado ?>"  data-oferta="<?= $img->oferta ?>" data-id="<?= $dis->id ?>" data-code="<?= $dis->name ?>" id="modal-img<?= $dis->id ?>" style="background: center center  url('<?= $dis->getUrl('original') ?>') no-repeat; background-size: cover;" class="detail-full-item-modal"></div>
+                                    <?= $this->render('_modelos_div', ['disenio' => $img]) ?>
                                 </div>
-                                <h6 class="text-center" id="cant-modelos"></h6>
+                                <h6 class="text-center cant-modelos" id="cant-modelos-<?= $dis->id ?>"></h6>
+
                             </div>
                             <div class="col-lg-6 d-flex align-items-center">
                                 <div>
                                     <h3 id="" class="modal-disenio-codigo  mt-2 mt-lg-1 font-alt">
-                                        <?= $lisos->name ." ".$lisos->description ?>
+                                        <?= $dis->name. " " .$dis->description ?>
                                     </h3>
                                     <h2 id="" class="modal-tela-nombre mb-2  mt-lg-1 text-tiles">
                                         <?= $model->getNombreCompleto() ?>
@@ -49,18 +45,18 @@
                                         <?= $model->descripcion_larga_tela ?>                                                           
                                     </p>
                                     <form action="#">
-                                        <input class="modal-disenio-id" name="id" value="<?= $lisos->id ?>" type="hidden"></input>
+                                        <input class="modal-disenio-id" name="id" value="<?= $dis->id ?>" type="hidden"></input>
                                         <div class="row">
 
                                             <div class="col-12 detail-option mb-3">
                                                 <label class="detail-option-heading font-weight-bold">Cantidad </label>
-                                                <input name="cantidad" type="number" value="1" class="form-control detail-quantity">
+                                                <input name="cantidad" type="number" value="1" class="modal-cantidad form-control detail-quantity">
                                             </div>
                                         </div>
                                         <ul class="list-inline">
                                             <li class="list-inline-item">
 
-                                                <button id="submit-consulta" type="button" class="submit-zoom btn btn-dark btn-lg mb-1">Consultar<i class="fa fa-chevron-right"></i></button>
+                                                <button id="submit-consulta" type="button" class="submit-zoom btn btn-dark btn-lg mb-1"> Consultar<i class="fa fa-chevron-right"></i></button>
                                             </li>
 
                                         </ul>
@@ -75,4 +71,4 @@
 
 
     <?php endforeach; ?>
-<?php endif; ?>
+<?php endforeach; ?>
