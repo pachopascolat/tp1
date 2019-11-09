989de0a5ec051bc11afa90b6b05d96aa61727256
pachopascolat <patriciopascolat@gmail.com>
2019-11-08 23:55:39 -0300

    agregado menu para celular fix logo y container

--- frontend/views/texsim/_carousel.php
+++ frontend/views/texsim/_carousel.php
@@ -7,16 +7,16 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100" src="img/header-fototexsim-03.jpg" alt="">
+            <img class="d-block" src="img/header-fototexsim-03.jpg" alt="">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="img/header-fototexsim-04.jpg" alt="">
+            <img class="d-block" src="img/header-fototexsim-04.jpg" alt="">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="img/header-fototexsim-05.jpg" alt="">
+            <img class="d-block" src="img/header-fototexsim-05.jpg" alt="">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="img/header-fototexsim-06.jpg" alt="">
+            <img class="d-block" src="img/header-fototexsim-06.jpg" alt="">
         </div>
     </div>
 </div>
