64349aac2811c0166a35509979a3e8195e407090
pachopascolat <patriciopascolat@gmail.com>
2020-01-13 00:16:26 -0300

    nav movil modal

--- frontend/views/layouts/nav1.php
+++ frontend/views/layouts/nav1.php
@@ -14,7 +14,7 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
             <div class="flex-fill busqueda-div d-none d-md-block">
                 <?= \yii\helpers\Html::beginForm(['/sitio/buscar'], 'GET', ['id' => 'busqueda-form', 'class' => 'navbar-link flex-fill d-flex align-items-center']);
                 ?>
-            <!--<form id="busqueda-form" method="POST" action="<?php // echo \yii\helpers\Url::to(['/sitio/buscar'])             ?>" class="navbar-link flex-fill d-flex ">-->
+            <!--<form id="busqueda-form" method="POST" action="<?php // echo \yii\helpers\Url::to(['/sitio/buscar'])                        ?>" class="navbar-link flex-fill d-flex ">-->
                 <input value="<?= $_GET['busqueda'] ?? '' ?>" id="busqueda" name="busqueda" class="texsim-search-input w-100" type="" placeholder="" aria-label="Search">
                 <a href="" onclick="$('#busqueda-form').submit()"> 
                     <img src="<?= \yii\helpers\Url::base(true) ?>/img2020/lupa-01.svg" class="lupa-icon">
@@ -71,64 +71,48 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
 
 <div class="d-lg-none">
     <nav class="navbar-movil navbar navbar-texsim navbar-dark nav1">
-        <div class="container">
-            <div id="movil-search-input" class="w-100 collapse ">
-                <form method="POST" action="<?= \yii\helpers\Url::to(['/texsim/buscar-telas']) ?>"  class="form-inline my-2 my-lg-0 navbar-link flex-fill">
-                    <input id="busqueda-movil" name="busqueda" class="form-control texsim-search-input nav-item w-100" type="search" placeholder="" aria-label="Search">
-                    <!--<i class="fa fa-search text-light"></i>-->
-                    <img src="" class="lupa-icon"></img>
-                    <img src="<?= \yii\helpers\Url::base(true) . "/img/lupa-01.svg" ?>" class="lupa-icon"></img>
-                    <!--<img src="img/lupa-01.svg" class="lupa-icon"></img>-->
-                    <!--<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
-                </form>
-            </div>
-            <div class="w-100 d-flex align-items-center">
-                <a class="navbar-brand" href="/">
+        <!--<div class="">-->
+        <div class="d-flex align-items-center w-100">
+            <!--<div class="movil-logo-div">-->
+                <a class="movil-logo-div navbar-brand" href="/">
                     <img class="img-fluid" src="<?= \yii\helpers\Url::base(true) . "/img/logotexsim-02.svg" ?>">
-                    <!--<img class="img-fluid" src="img/logotexsim-02.svg">-->
-
                 </a>
-                <div class="d-flex w-100 justify-content-end">
-
-                    <!-- Cart Dropdown-->
-                    <?php
-                    $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
-                    ?>
-                    <div class="d-flex align-items-center" >
-
-                        <div class="carrito-count-div  <?= $carrito && $carrito->itemCarritos ? '' : 'd-none' ?>">
-                            <div class="navbar-icon-link2 carrito-link  ">
-                                <a  data-pjax=0
-                                    href="<?= yii\helpers\Url::to(['crear-consulta']) ?>" 
-                                    class="navbar-icon-link ">
-
-                                    <img class="header-icon" src="<?= \yii\helpers\Url::base(true) . "/img/txsim-header-consulta-01.svg" ?>" alt="listado">
-                                    <!--<img class="header-icon" src="img/txsim-header-consulta-01.svg" alt="listado">-->
-                                    <div class="navbar-icon-link-badge carrito-count"><?= $carrito ? count($carrito->itemCarritos) : '' ?></div>
-
-                                </a>
-                                <div class="tooltiptext tooltiptext-link">
-                                    Haga click aquí para ver su consulta
-                                </div>
-                                <div class="tooltiptext tooltiptext-notice">
-                                    Su consulta se ha guardado aquí
-                                </div>
-
-                            <!--<span class="d-xs-block d-sm-none p-2">Ir a Consulta</span>-->
-                            </div>
-
+            <!--</div>-->
+            <!--<div class="">-->
+            <!--<div class="d-flex">-->
+            <div class="flex-fill d-flex align-items-center justify-content-center">
+                <div class="nav-item text-light tel-movil">
+                    <span>(54 11) 2120-0550</span>
+                </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+                <!-- Cart Dropdown-->
+                <?php
+                $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
+                ?>
+                <div class="pl-2 pr-2 carrito-count-div  <?= $carrito && $carrito->itemCarritos ? '' : 'd-none' ?>">
+                    <div class="navbar-icon-link2 carrito-link  ">
+                        <div class="d-flex carrito-icon-div">
+                            <a  data-pjax=0
+                                href="<?= yii\helpers\Url::to(['crear-consulta']) ?>" 
+                                class="">
+                                <i class=" text-light fal fa-shopping-cart fa-2x"></i>
+                            </a>
+                            <div class="navbar-icon-link-badge carrito-count"><?= $carrito ? count($carrito->itemCarritos) : '' ?></div>
+                        </div>
+                        <div class="tooltiptext tooltiptext-link">
+                            Haga click aquí para ver su consulta
+                        </div>
+                        <div class="tooltiptext tooltiptext-notice">
+                            Su consulta se ha guardado aquí
                         </div>
                     </div>
-
-                    <button class="navbar-toggler" type="button" data-toggle="modal" data-target="#sidebar-left" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
                 </div>
-                <!-- Sidebar Left -->
+                <a class="" type="button" data-toggle="modal" data-target="#sidebar-left" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <!--<span class="navbar-toggler-icon"></span>-->
+                    <i class="text-light fal fa-bars fa-2x"></i>
+                </a>
 
-                <!--                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                
-                                    </div>-->
             </div>
         </div>
     </nav>
@@ -148,7 +132,7 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
                 </a>
             </div>
             <div class="modal-body">
-                
+
                 <div class="pb-3  borde-movil-gris">
                     <ul class="navbar-nav">
 
@@ -202,7 +186,7 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
                         <ul class="navbar-nav">
 
                             <?php
-                            $vidrieras = \common\models\Vidriera::find()->limit(5)->all();
+                            $vidrieras = \common\models\Vidriera::find()->all();
                             foreach ($vidrieras as $vidriera):
                                 ?>
                                 <li class="nav-item item-categoria">
@@ -251,8 +235,8 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
 
                 <div class="side-menu-login nav-item">
                     <?php if (Yii::$app->user->isGuest) { ?>
-                    <a href="" class="side-menu-link text-white cursor-pointer" data-toggle="modal" data-target="#login-modal">
-                            <!--<i style="" class="fa fa-lock "></i>-->
+                        <a href="" class="side-menu-link text-white cursor-pointer" data-toggle="modal" data-target="#login-modal">
+                                <!--<i style="" class="fa fa-lock "></i>-->
                             <span class="movil-categorias">LOGIN</span>
                             <!--<span class="d-xs-block d-sm-none">Login</span>-->
 
