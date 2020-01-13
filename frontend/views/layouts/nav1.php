de640a3c0a5a55a9212d6df0d19928b1a5fa7edd
pachopascolat <patriciopascolat@gmail.com>
2020-01-12 23:14:39 -0300

    nav movil modal

--- frontend/views/layouts/nav1.php
+++ frontend/views/layouts/nav1.php
@@ -1,7 +1,7 @@
 <?php
 $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
 ?>
-<nav class="nav1 navbar sticky-top">
+<nav class="nav1 navbar sticky-top d-none d-lg-block">
     <div class="container">
         <div class="w-100 d-flex  align-items-center">
             <div class="logo">
@@ -14,7 +14,7 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
             <div class="flex-fill busqueda-div d-none d-md-block">
                 <?= \yii\helpers\Html::beginForm(['/sitio/buscar'], 'GET', ['id' => 'busqueda-form', 'class' => 'navbar-link flex-fill d-flex align-items-center']);
                 ?>
-            <!--<form id="busqueda-form" method="POST" action="<?php // echo \yii\helpers\Url::to(['/sitio/buscar'])     ?>" class="navbar-link flex-fill d-flex ">-->
+            <!--<form id="busqueda-form" method="POST" action="<?php // echo \yii\helpers\Url::to(['/sitio/buscar'])             ?>" class="navbar-link flex-fill d-flex ">-->
                 <input value="<?= $_GET['busqueda'] ?? '' ?>" id="busqueda" name="busqueda" class="texsim-search-input w-100" type="" placeholder="" aria-label="Search">
                 <a href="" onclick="$('#busqueda-form').submit()"> 
                     <img src="<?= \yii\helpers\Url::base(true) ?>/img2020/lupa-01.svg" class="lupa-icon">
@@ -68,3 +68,216 @@ $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
         </div>
     </div>   
 </nav> 
+
+<div class="d-lg-none">
+    <nav class="navbar-movil navbar navbar-texsim navbar-dark nav1">
+        <div class="container">
+            <div id="movil-search-input" class="w-100 collapse ">
+                <form method="POST" action="<?= \yii\helpers\Url::to(['/texsim/buscar-telas']) ?>"  class="form-inline my-2 my-lg-0 navbar-link flex-fill">
+                    <input id="busqueda-movil" name="busqueda" class="form-control texsim-search-input nav-item w-100" type="search" placeholder="" aria-label="Search">
+                    <!--<i class="fa fa-search text-light"></i>-->
+                    <img src="" class="lupa-icon"></img>
+                    <img src="<?= \yii\helpers\Url::base(true) . "/img/lupa-01.svg" ?>" class="lupa-icon"></img>
+                    <!--<img src="img/lupa-01.svg" class="lupa-icon"></img>-->
+                    <!--<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
+                </form>
+            </div>
+            <div class="w-100 d-flex align-items-center">
+                <a class="navbar-brand" href="/">
+                    <img class="img-fluid" src="<?= \yii\helpers\Url::base(true) . "/img/logotexsim-02.svg" ?>">
+                    <!--<img class="img-fluid" src="img/logotexsim-02.svg">-->
+
+                </a>
+                <div class="d-flex w-100 justify-content-end">
+
+                    <!-- Cart Dropdown-->
+                    <?php
+                    $carrito = \common\models\Carrito::findOne($_SESSION['carrito']);
+                    ?>
+                    <div class="d-flex align-items-center" >
+
+                        <div class="carrito-count-div  <?= $carrito && $carrito->itemCarritos ? '' : 'd-none' ?>">
+                            <div class="navbar-icon-link2 carrito-link  ">
+                                <a  data-pjax=0
+                                    href="<?= yii\helpers\Url::to(['crear-consulta']) ?>" 
+                                    class="navbar-icon-link ">
+
+                                    <img class="header-icon" src="<?= \yii\helpers\Url::base(true) . "/img/txsim-header-consulta-01.svg" ?>" alt="listado">
+                                    <!--<img class="header-icon" src="img/txsim-header-consulta-01.svg" alt="listado">-->
+                                    <div class="navbar-icon-link-badge carrito-count"><?= $carrito ? count($carrito->itemCarritos) : '' ?></div>
+
+                                </a>
+                                <div class="tooltiptext tooltiptext-link">
+                                    Haga click aquí para ver su consulta
+                                </div>
+                                <div class="tooltiptext tooltiptext-notice">
+                                    Su consulta se ha guardado aquí
+                                </div>
+
+                            <!--<span class="d-xs-block d-sm-none p-2">Ir a Consulta</span>-->
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <button class="navbar-toggler" type="button" data-toggle="modal" data-target="#sidebar-left" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <!-- Sidebar Left -->
+
+                <!--                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
+                                    </div>-->
+            </div>
+        </div>
+    </nav>
+</div>
+<div class="modal fade left modal-sidebar" id="sidebar-left" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content nav1">
+            <div class="modal-header border-0 pb-2">
+                <div class="pb-2 ">
+                    <a class="navbar-brand" href="">
+                        <img class="w-100  p-0 " src="<?= \yii\helpers\Url::base(true) . "/img/logotexsim-02.svg" ?>">
+
+                    </a>
+                </div>
+                <a href="" class="close p-3" data-dismiss="modal" aria-label="Close">
+                    <i class="text-white fal fa-times fa-1x"></i>
+                </a>
+            </div>
+            <div class="modal-body">
+                
+                <div class="pb-3  borde-movil-gris">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item item-categoria-padre">
+                            <a class="pb-1 pt-0" href="<?= \yii\helpers\Url::to(['/sitio/por-categoria', 'id_categoria' => 1]) ?>" id="" >
+                                <h6 class="text-moda">MODA</h6>
+                            </a>
+                        </li>
+                        <?php
+                        $categorias = \common\models\Categoria::find()->where(['moda' => true])->all();
+                        foreach ($categorias as $categoria):
+                            ?>
+                            <li class="nav-item item-categoria">
+                                <a class="nav-link p-0" data-categoria="<?= $categoria->id_categoria ?>"  href="<?= yii\helpers\Url::to(['por-categoria', 'id_categoria' => $categoria->id_categoria]) ?>">
+                                    <span class="movil-categorias"><?= $categoria->nombre_categoria ?></span>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+
+
+                    </ul>
+                </div>
+                <div class=" borde-movil-gris pb-3 pt-3 ">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item item-categoria-padre">
+                            <a class="pb-1 movil-categoria-padre" href="<?= \yii\helpers\Url::to(['/sitio/por-categoria', 'id_categoria' => 2]) ?>" id="" >
+                                <h6 class="text-hogar">HOGAR</h6>
+                            </a>
+                        </li>
+                        <?php
+                        $categorias = \common\models\Categoria::find()->where(['hogar' => true])->all();
+                        foreach ($categorias as $categoria):
+                            ?>
+                            <li class="nav-item item-categoria">
+                                <a class="nav-link p-0" data-categoria="<?= $categoria->id_categoria ?>"  href="<?= yii\helpers\Url::to(['por-categoria', 'id_categoria' => $categoria->id_categoria]) ?>">
+                                    <span class="movil-categorias"><?= $categoria->nombre_categoria ?></span>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+
+
+                    </ul>
+                </div>
+                <div class="borde-movil-gris pb-3 pt-3 ">
+                    <a href="" class="w-100 d-flex justify-content-between align-items-center" data-target="#side-menu-telas" id="" role="button" data-toggle="collapse" >
+                        <span class="movil-categorias">Telas</span>
+                        <i class="movil-categorias fa fa-chevron-down"></i>
+                    </a>
+                    <div id="side-menu-telas" class="collapse pt-2">
+                        <ul class="navbar-nav">
+
+                            <?php
+                            $vidrieras = \common\models\Vidriera::find()->limit(5)->all();
+                            foreach ($vidrieras as $vidriera):
+                                ?>
+                                <li class="nav-item item-categoria">
+                                    <a class="nav-link p-0" href="<?= yii\helpers\Url::to(['por-vidriera', 'id' => $vidriera->id_vidriera]) ?>">
+                                        <span class="movil-categorias text-light"><?= $vidriera->nombre ?></span>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="borde-movil-gris side-menu-social-iconos pt-3 pb-3">
+                    <a class="mr-3"  href="instagram.com/texsim"><i class="fab fa-instagram text-light"></i></a>
+                    <a class="mr-3" href="facebook.com/texsim"><i class="fab fa-facebook text-light"></i></a>
+                    <a class="mr-3" target="_blanc" href="https://api.whatsapp.com/send?phone=541135386219&text=Me%20gustar%C3%ADa%20saber%20mas%20sobre%20sus%20productos.%20Gracias&source=&data=#"><i class="fab fa-whatsapp text-light"></i></a>
+
+                </div>
+                <div class="nav-item text-white pt-3">
+                    <a>
+                        <span class="movil-categorias">
+                            CONSULTAS  
+                        </span>
+                        <span class="text-light">2120 0550</span>
+                    </a>
+                </div>
+                <div class="nav-item side-menu-sucursales ">
+                    <a href="" class="w-100 d-flex justify-content-between align-items-center side-menu-link cursor-pointer text-blue d-inline" data-target="#side-menu-suc-dir" id="" role="button" data-toggle="collapse" >
+                        <span class="movil-categorias">sucursales</span>
+                        <i class="movil-categorias fa fa-chevron-down"></i>
+                    </a>
+
+                    </a>
+
+
+                    <div id="side-menu-suc-dir" class="collapse dir-sucursales text-light" aria-labelledby="navbarDropdown">
+                        <div><span class="">Lavalle 2571 / 2120 0550</span> </div>
+                        <div><span class="">Azcuenaga 580 / 2120 0580</span></div>
+                        <div><span class="">Olavarria 2348 / 6072 6831</span></div>
+                        <div><span class="">Villa Celina</span></div>
+                    </div>
+
+                </div>
+
+                <div class="side-menu-login nav-item">
+                    <?php if (Yii::$app->user->isGuest) { ?>
+                    <a href="" class="side-menu-link text-white cursor-pointer" data-toggle="modal" data-target="#login-modal">
+                            <!--<i style="" class="fa fa-lock "></i>-->
+                            <span class="movil-categorias">LOGIN</span>
+                            <!--<span class="d-xs-block d-sm-none">Login</span>-->
+
+                        </a>
+
+                        <?php
+                    } else {
+                        echo '<i style="" class="fa fa-lock header-icon d-xs-block d-sm-none "></i>';
+                        echo \yii\helpers\Html::beginForm(['/user/logout'], 'post');
+                        ?>
+                        <button name="logout" class="btn text-light logout-btn" onclick="$('form').submit()">
+                            <span>Logout(<?= Yii::$app->user->identity->username ?>)</span>
+                        </button>
+                        <?php
+//                                echo \yii\helpers\Html::submitButton(
+////                                        'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn text-white word-break', 'name' => 'logout']
+//                                        'Logout', ['class' => 'text-white nav-link p-0 ml-1', 'name' => 'logout']
+//                                );
+                        echo \yii\helpers\Html::endForm();
+                    }
+                    ?>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
