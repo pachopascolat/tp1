abcb55ab0b06abea8f88b707cbd57b2a05dc4d4e
pachopascolat <patriciopascolat@gmail.com>
2020-01-13 03:21:20 -0300

    nav movil modal

--- frontend/views/sitio/_modalItem.php
+++ frontend/views/sitio/_modalItem.php
@@ -21,11 +21,11 @@
                         <div class="row">
                             <div class="col-lg-6 p-0">
                                 <div class="d-flex justify-content-center">
-                                    <img class="img-fluid w-100 lazy_load" data-src="<?= $url ?>">
+                                    <img class="img-modal-item img-fluid w-100 lazy_load" data-src="<?= $url ?>">
                                 </div>
                             </div>
-                            <div class="col-lg-6 d-flex align-items-center pl-lg-5">
-                                <div class="">
+                            <div class="col-lg-6 d-flex align-items-center pl-lg-5 text-center text-lg-left">
+                                <div class="w-100">
                                     <h3 id="" class="modal-disenio-codigo  mt-2 mt-lg-1 title">
 
                                         <?= "{$item->articulo->codigo_color} {$item->articulo->nombre_color} " ?>
@@ -34,7 +34,7 @@
                                         <?= $item->articulo->tela->nombre_tela ?>
                                     </h2>
 
-                                    <p id="" class="modal-descripcion mb-4 text-muted title">
+                                    <p id="" class="modal-descripcion mb-md-4 text-muted title">
                                         <?= $item->articulo->tela->descripcion_larga_tela ?>
 
                                     </p>
@@ -42,8 +42,8 @@
                                         <input class="modal-disenio-id" name="id" value="<?= $item->articulo_id ?>" type="hidden">
                                         <div class="">
 
-                                            <div class=" mb-3">
-                                                <label class="title detail-option-heading font-weight-bold">Cantidad </label>
+                                            <div class=" mb-3 d-flex d-lg-block justify-content-center align-items-center">
+                                                <label class="title detail-option-heading font-weight-bold mb-0 pr-3 pt-1 pb-1">Cantidad </label>
                                                 <input name="cantidad" type="number" value="1" class="modal-cantidad form-control detail-quantity">
                                             </div>
                                         </div>
