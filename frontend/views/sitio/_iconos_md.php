f78e2d04334ce50a4d7aef7fb0e4065c04195619
pachopascolat <patriciopascolat@gmail.com>
2020-01-24 11:37:08 -0300

    cambiar vidriera iconos en front

--- frontend/views/sitio/_iconos_md.php
+++ frontend/views/sitio/_iconos_md.php
@@ -1,70 +1,26 @@
+<?php
+$iconos = common\models\Vidriera::find()->where(['categoria_id' => common\models\Categoria::ICONOS])->limit(12)->all();
+$iconosFilas = array_chunk($iconos, 6);
+?>
+
 <div class="dosFilasIconos mt-5">
     <div class="container">
         <h3>Mas telas..</h3>
-         <!-- row 1 -->
-        <div class="row">
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2709])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-ketten-01.svg" alt="ketten">
-                </a>
+        <!-- row 1 -->
+        <?php foreach ($iconosFilas as $fila) : ?>
+            <div class="row">
+                <?php foreach ($fila as $vidriera) : ?>
+                    <div class="col ">                                
+                        <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera', 'id' => $vidriera->id_vidriera]) ?>">
+                            <div class="icono-div">
+                                <img alt="icono" class="lazy w-100" data-src="<?= \Yii::$app->imagemanager->getImagePath($vidriera->imagen_id) ?>" alt="<?=$vidriera->nombre?>">
+                                <span class="w-100 icono-nombre text-center"><?= $vidriera->nombre ?></span>
+                            </div>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+
             </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2710])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-batista-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2711])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-jean-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2712])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-cuerina-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2713])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-sedafria-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2714])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-modal-01.svg" alt="ketten">
-                </a>
-            </div>
-        </div>     <!-- row 1 end -->
-        <div class="row">  <!-- row 2 -->
-            <div class="col">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2717])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-tapiceria-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2718])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-fibrana-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2719])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-microfibra-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2720])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-promos-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2722])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-poliester-01.svg" alt="ketten">
-                </a>
-            </div>
-            <div class="col ">                                
-                <a href="<?= \yii\helpers\Url::to(['/sitio/por-vidriera','id'=>2721])?>">
-                    <img alt="icono" class="lazy" data-src="<?= yii\helpers\Url::base(true)?>/img2020/texsim-plastico-01.svg" alt="ketten">
-                </a>
-            </div>
-        </div>    <!-- row 2 end -->   
-    </div> <!-- container end -->
+        <?php endforeach; ?>
+</div> <!-- container end -->
 </div>
