16554e550f2a0ebd76089b3708346769ecdd731b
pachopascolat <patriciopascolat@gmail.com>
2020-08-07 17:33:44 -0300

    tabla tamaño letra

--- backend/views/chart/Components/Grafico.php
+++ backend/views/chart/Components/Grafico.php
@@ -89,7 +89,7 @@
             },
             drawChart: function(){
                 var cantidades = this.cantidades;
-                cantidades.push(0);
+                // cantidades.push(0);
                 var fechas = this.fechas;
                 // console.log(this.$refs['mychart']);
                 var ctx = this.$refs.myChart.getContext('2d');
