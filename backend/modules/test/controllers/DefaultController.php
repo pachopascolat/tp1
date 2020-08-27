46d1201ea771ff8e095c443a20ac7145bc7c105d
pachopascolat <patriciopascolat@gmail.com>
2020-08-27 12:57:21 -0300

    tabla tamaño letra

--- backend/modules/test/controllers/DefaultController.php
+++ backend/modules/test/controllers/DefaultController.php
@@ -15,6 +15,6 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect('/estado-pedido/index');
     }
 }
